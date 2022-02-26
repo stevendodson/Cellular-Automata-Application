684e2e8695a928d93b71d5ef0c284b4003edad37
Steven Dodson <steven.m.dodson-1@ou.edu>
2022-02-26 15:55:21 -0600

    Generation class passed all tests

--- src/Generation.java
+++ src/Generation.java
@@ -1,8 +1,20 @@
 import java.util.Arrays;
 
+/**
+ * Represents the cells of a one-dimensional, two-state cellular automata at a
+ * single moment in time.
+ * 
+ * @author Steven Dodson
+ * @version 1.0
+ */
 public class Generation {
 	private boolean[] cellStates;
 
+	/**
+	 * Construct a Generation with the given states
+	 * 
+	 * @param states list of states for the Generation
+	 */
 	public Generation(boolean... states) {
 		if (states == null || states.length == 0) {
 			cellStates = new boolean[1];
@@ -12,6 +24,13 @@ public class Generation {
 		cellStates = Arrays.copyOf(states, states.length);
 	}
 
+	/**
+	 * Construct a Generation using the given string and a character to represent
+	 * true.
+	 * 
+	 * @param states     string to be parsed and used to create a Generation
+	 * @param trueSymbol character to represent true
+	 */
 	public Generation(String states, char trueSymbol) {
 		if (states == null || states == "") {
 			cellStates = new boolean[1];
@@ -28,14 +47,33 @@ public class Generation {
 		}
 	}
 
+	/**
+	 * Return the state of the given index in Generation.
+	 * 
+	 * @param index the index of the desired element
+	 * @return the boolean value at the given index
+	 */
 	public boolean getState(int index) {
 		return cellStates[index];
 	}
 
+	/**
+	 * Returns an array containing the states of every cell in the Generation object.
+	 * 
+	 * @return a boolean array containing the states of each cell in the Generation
+	 *         object
+	 */
 	public boolean[] getStates() {
 		return Arrays.copyOf(cellStates, size());
 	}
 
+	/**
+	 * Returns a String representation of the Generation object with the given true symbol and false symbol.
+	 * 
+	 * @param falseSymbol character to be used to represent false
+	 * @param trueSymbol character to be used to represent true
+	 * @return a String representation of the Generation object with the given true symbol and false symbol
+	 */
 	public String getStates(char falseSymbol, char trueSymbol) {
 		StringBuilder str = new StringBuilder();
 		for (int i = 0; i < size(); ++i) {
@@ -48,6 +86,10 @@ public class Generation {
 		return str.toString();
 	}
 
+	/**
+	 * Returns the number of cells in the Generation object.
+	 * @return the number of cells in the Generation object
+	 */
 	public int size() {
 		return cellStates.length;
 	}

c52f3b08921a5c0d70ebffc4a24a2b09db43ed2e
Steven Dodson <steven.m.dodson-1@ou.edu>
2022-02-26 22:10:13 -0600

    Created method stubs for TotalisticRule

--- src/TotalisticRule.java
+++ src/TotalisticRule.java
@@ -1,4 +1,16 @@
 
-public class TotalisticRule {
-
+public class TotalisticRule extends Rule {
+	protected TotalisticRule(int ruleNum) {
+		super(ruleNum);
+		
+	}
+	public boolean evolve(boolean[] neighborhood) {
+		return false;
+	}
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		return null;
+	}
+	public String ruleTableString(char falseSymbol, char trueSymbol) {
+		return null;
+	}
 }

c7c8259e83675dc0aae720241f3d6d8bcbe3af27
Steven Dodson <steven.m.dodson-1@ou.edu>
2022-02-28 13:20:21 -0600

    TotalisticRule passed all tests

--- src/TotalisticAutomaton.java
+++ src/TotalisticAutomaton.java
@@ -1,10 +1,17 @@
+import java.io.IOException;
 
 public class TotalisticAutomaton extends Automaton {
 
+	protected TotalisticAutomaton(int ruleNum, Generation initial) throws RuleNumException {
+		super(ruleNum, initial);
+	}
+	protected TotalisticAutomaton(String filename) throws NumberFormatException, IOException, RuleNumException {
+		super(filename);
+	}
+
 	@Override
-	protected Rule createRule(int ruleNum) {
-		// TODO Auto-generated method stub
-		return null;
+	protected Rule createRule(int ruleNum) throws RuleNumException {
+		return new TotalisticRule(ruleNum);
 	}
 
 }
